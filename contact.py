33c804b92488939d51061b9b86893efd1705197e
Marina Belousova <belousowa.marina14@gmail.com>
2020-08-18 19:19:25 +0300

    Выделение вспомогательных методов и классов для контакта

---
+++ contact.py
@@ -0,0 +1,12 @@
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nick, mail, bday_day, bday_month, bday_year):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nick = nick
+        self.mail = mail
+        self.bday_day = bday_day
+        self.bday_month = bday_month
+        self.bday_year = bday_year
