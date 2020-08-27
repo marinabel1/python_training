9af652b8e7dc715df371a66622d39745a4b8a4b3
Marina Belousova <belousowa.marina14@gmail.com>
2020-08-27 21:40:53 +0300

    Сделать все тесты создания, удаления, модификации для групп и контактов

---
+++ test/test_del_contact.py
@@ -0,0 +1,12 @@
+
+
+def test_delete_first_contact(app):
+    app.session.login( username="admin", password="secret")
+    app.contact.delete_first_contact()
+    app.session.logout()
+
+
+def test_delete_all_contact(app):
+    app.session.login( username="admin", password="secret")
+    app.contact.delete_all_contact()
+    app.session.logout()
